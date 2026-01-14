195669ceae6b96e6f161af291e98bc1ae226a17f
BenHavis <ben.havis1@gmail.com>
2026-01-14 15:11:43 -0500

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import Navbar from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
            <Navbar />
         <AntdRegistry>{children}</AntdRegistry>
+         <Analytics />
       </body>
     </html>
   );
